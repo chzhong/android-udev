885dc82ebd92fa83e31657fb0a844b380c2b1902
Cheng Zhong <chzhong@bainainfo.com>
2016-08-02 19:27:06 +0800

    修正 udevd 的编译

--- jni/android/missing.c
+++ jni/android/missing.c
@@ -62,3 +62,31 @@ int sched_getaffinity(pid_t pid, size_t setsize, cpu_set_t* set)
 {
 	return syscall(__NR_sched_getaffinity, pid, setsize, set);
 }
+
+typedef unsigned long int __cpu_mask;
+
+int __sched_cpucount(size_t setsize, cpu_set_t* cpuset)
+{ 
+    int count = 0; 
+    const __cpu_mask *p = cpuset->__bits; 
+    // Note that we assume, like glibc's implementation does, that setsize 
+    // is a multiple of sizeof(__cpu_mask). The CPU_ALLOC_SIZE() macro indeed 
+    // always returns a multiple of sizeof(__cpu_mask). 
+    const __cpu_mask *end = p + (setsize / sizeof(__cpu_mask)); 
+ 
+    while (p < end) { 
+        count +=  __builtin_popcountl(*p++); 
+    } 
+ 
+    return count; 
+} 
+ 
+cpu_set_t *__sched_cpualloc (size_t count) 
+{ 
+    return (cpu_set_t*) malloc (CPU_ALLOC_SIZE (count)); 
+} 
+ 
+void __sched_cpufree (cpu_set_t *set) 
+{ 
+    free (set); 
+} 
